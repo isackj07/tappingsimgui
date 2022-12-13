4c73607281d2487d3efb4a23beabd8a6367fcc71
Isackj07 <95695326+isackj07@users.noreply.github.com>
2022-12-13 23:37:50 +0800

    Update TappingSimulator Gui.lua

--- TappingSimulator Gui.lua
+++ TappingSimulator Gui.lua
@@ -1,12 +1,17 @@
 _G.autoTap = true;
 _G.autoRebirth = true;
 _G.autoStarterEgg = true;
+local remotePath = game:GetService("ReplicatedStorage").Events;
+
+for i, v in pairs(_G) do
+    print(i, v);
+end
 
 
 spawn(function()
     while _G.autoTap == true do
         local A_1 = 1
-        local Event = game:GetService("ReplicatedStorage").Events.Tap
+        local Event = remotePath.Tap
         Event:FireServer(A_1)
         wait()
     end
@@ -15,7 +20,7 @@ end)
 spawn(function()
     while _G.autoRebirth == true do
         local A_1 = 1
-        local Event = game:GetService("ReplicatedStorage").Events.Rebirth
+        local Event = remotePath.Rebirth
         Event:FireServer(A_1)
         wait()
     end
@@ -23,7 +28,7 @@ end)
 
 spawn(function()
     while _G.autoStarterEgg == true do
-        local args = { [1] = {}, [2] = "Starter", [3] = 1 } game:GetService("ReplicatedStorage").Events.HatchEgg:InvokeServer(unpack(args))
+        local args = { [1] = {}, [2] = "Starter", [3] = 1 } remotePath.HatchEgg:InvokeServer(unpack(args))
         wait()
     end
 end)
