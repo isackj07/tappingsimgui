334cbc4491d5b2eb988e793ea80e1435493c0a6a
Isackj07 <95695326+isackj07@users.noreply.github.com>
2022-12-19 23:06:22 +0800

    Update TappingSimulator Gui.lua

--- TappingSimulator Gui.lua
+++ TappingSimulator Gui.lua
@@ -1,34 +1,40 @@
 getgenv().autoTap = true; --Global variable, getgenv() = get global environment
 getgenv().autoRebirth = true; --getngenv() is safer than _G
-getgenv().autoStarterEgg = true; --getgenv() is hidden from the game
+getgenv().autoEgg = true; --getgenv() is hidden from the game
 local remotePath = game:GetService("ReplicatedStorage").Events;
 
 for i, v in pairs(_G) do -- for loop, loops the _G table
     print(i, v);
 end
 
+function doTap()
+    spawn(function()
+        while autoTap == true do
+            local A_1 = 1
+            local Event = remotePath.Tap
+            Event:FireServer(A_1)
+            wait()
+        end
+    end)
+end
 
-spawn(function()
-    while autoTap == true do
-        local A_1 = 1
-        local Event = remotePath.Tap
-        Event:FireServer(A_1)
-        wait()
-    end
-end)
+function autoRebirth(rebirthAmount)
+    spawn(function()
+        while autoRebirth == true do
+            local A_1 = rebirthAmount
+            local Event = remotePath.Rebirth
+            Event:FireServer(A_1)
+            wait()
+        end
+    end)
+end
 
-spawn(function()
-    while autoRebirth == true do
-        local A_1 = 1
-        local Event = remotePath.Rebirth
-        Event:FireServer(A_1)
-        wait()
-    end
-end)
 
-spawn(function()
-    while autoStarterEgg == true do
-        local args = { [1] = {}, [2] = "Starter", [3] = 1 } remotePath.HatchEgg:InvokeServer(unpack(args))
-        wait()
-    end
-end)
+function autoEgg(eggType)
+    spawn(function()
+        while autoEgg == true do
+            local args = { [1] = {}, [2] = eggType, [3] = 1 } remotePath.HatchEgg:InvokeServer(unpack(args))
+            wait()
+        end
+    end)
+end
